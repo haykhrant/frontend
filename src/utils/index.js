99bbfa591216d43a0a69647e8236690a0f91c06d
hayk-muradyan <mhayk.973779@gmail.com>
2021-08-26 20:44:54 +0400

    change categories fields in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
 export const formatCategories = (categories) => {
-  return categories?.map((category) => ({
-    id: category.id,
-    name: category.categoryName,
-    subCategories: category.productTypeResponses?.map((subCategory) => ({
+  return categories?.map(({categoryResponse, productTypeResponses}) => ({
+    id: categoryResponse.id,
+    name: categoryResponse.categoryName,
+    subCategories: productTypeResponses?.map((subCategory) => ({
       id: subCategory.id,
       name: subCategory.productTypeName,
     })),
